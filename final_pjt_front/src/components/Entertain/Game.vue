e85dd90b6306dab17d8dcc6c287bb814e7555792
김민식 <a184562@gmail.com>
2023-05-22 13:51:21 +0900

    Game 기능 구현 완성

--- final_pjt_front/src/components/Entertain/Game.vue
+++ final_pjt_front/src/components/Entertain/Game.vue
@@ -3,9 +3,15 @@
 		<div class="container">
 			<div @click="selectCard_left">
 				<GameCard class="Card" :movie="movie_1" />
+				<div id="popularity_1">
+					0
+				</div>
 			</div>
 			<div @click="selectCard_right">
 				<GameCard class="Card" :movie="movie_2" />
+				<div id="popularity_2">
+					0
+				</div>
 			</div>
 		</div>
 		
@@ -32,6 +38,10 @@ export default {
 			idx_1 : 0,
 			idx_2 : 1,
 			score : 0,
+			counting_num_1 : 0,
+			counting_num_2 : 0,
+			
+
 		}
 	},
 	created() {
@@ -44,7 +54,8 @@ export default {
 		
 		this.movie_1 = this.movie_list[this.idx_1]
 		this.movie_2 = this.movie_list[this.idx_2]
-
+		this.counting_num_1 = this.movie_1.popularity
+		this.counting_num_2 = this.movie_2.popularity
 		console.log(this.movie_1)
 		console.log(this.movie_2)
 	},
@@ -52,39 +63,103 @@ export default {
 		shuffle(array) {
 			array.sort(() => Math.random() -0.5)
 		},
-		selectCard_left() {
-			if (this.movie_1.popularity > this.movie_2.popularity) {
-				this.score += 1
-				this.idx_1 = this.idx_2
-				this.idx_2 += 1
-				this.movie_1 = this.movie_list[this.idx_1]
-				this.movie_2 = this.movie_list[this.idx_2]
-				console.log(this.movie_1)
-			}
-			else {
-				alert("틀렸습니다.")
-				this.$router.push({name: 'entertain'})
-			}
+		async selectCard_left() {
+			await this.countingNum_1(this.counting_num_1)
+
+			setTimeout(() => {
+				if (this.movie_1.popularity > this.movie_2.popularity) {
+					this.score += 1
+					this.idx_1 = this.idx_2
+					this.idx_2 += 1
+					this.movie_1 = this.movie_list[this.idx_1]
+					this.movie_2 = this.movie_list[this.idx_2]
+					console.log(this.movie_1)
+					this.counting_num_1 = this.movie_1.popularity
+					this.counting_num_2 = this.movie_2.popularity
+					document.querySelector("#popularity_1").innerHTML = 0
+					document.querySelector("#popularity_2").innerHTML = 0
+				}
+				else {
+					alert("틀렸습니다.")
+					this.$router.push({name: 'entertain'})
+				}
+			}, 1000)
+			
 		}
 		,
-		selectCard_right() {
-			if (this.movie_1.popularity < this.movie_2.popularity) {
-				this.score += 1
-				this.idx_1 = this.idx_2
-				this.idx_2 += 1
-				this.movie_1 = this.movie_list[this.idx_1]
-				this.movie_2 = this.movie_list[this.idx_2]
-				console.log(this.movie_1)
+		async selectCard_right() {
+			await this.countingNum_2(this.counting_num_2)
+
+			setTimeout(() => {
+				if (this.movie_1.popularity < this.movie_2.popularity) {
+					this.score += 1
+					this.idx_1 = this.idx_2
+					this.idx_2 += 1
+					this.movie_1 = this.movie_list[this.idx_1]
+					this.movie_2 = this.movie_list[this.idx_2]
+					console.log(this.movie_1)
+					this.counting_num_1 = this.movie_1.popularity
+					this.counting_num_2 = this.movie_2.popularity
+					document.querySelector("#popularity_1").innerHTML = 0
+					document.querySelector("#popularity_2").innerHTML = 0
+				}
+				else {
+					alert("틀렸습니다.")
+					this.$router.push({name: 'entertain'})
+				}
+			}, 1000)
+			
+		},
+		countingNum_1(num) {
+			const element = document.querySelector('#popularity_1')
+			if(num == 0) {
+				element.innerHTML = '0'
 			}
 			else {
-				alert("틀렸습니다.")
-				this.$router.push({name: 'entertain'})
+				const each = Math.ceil(num/25)
+				let time = 0
+			
+
+			for(let i = 0; i <= num; i += each) {
+				setTimeout(() => {
+					element.innerHTML = i
+				}, 20*time)
+				if(i+each > num) {
+					setTimeout(() => {
+						element.innerHTML = num
+					}, 20*(time+1))
+				}
+				time++
+			}}
+	},
+	countingNum_2(num) {
+			const element = document.querySelector('#popularity_2')
+			if(num == 0) {
+				element.innerHTML = '0'
 			}
+			else {
+				const each = Math.ceil(num/25)
+				let time = 0
+			
+
+			for(let i = 0; i <= num; i += each) {
+				setTimeout(() => {
+					element.innerHTML = i
+				}, 20*time)
+				if(i+each > num) {
+					setTimeout(() => {
+						element.innerHTML = num
+					}, 20*(time+1))
+				}
+				time++
+			}}
+		},
+		resetNum(num) {
+			num = 0
+			return num
 		}
-		
-	}
 	
-}
+}}
 </script>
 
 <style scoped>
--- final_pjt_front/src/components/Entertain/GameCard.vue
+++ final_pjt_front/src/components/Entertain/GameCard.vue
@@ -2,7 +2,6 @@
 	<div class="card">
 		<img :src="poster(movie.poster_path)" alt="">
 		<p>{{movie.title}}</p>
-		<p>{{ movie.popularity }}</p>
 	</div>
 </template>
 
@@ -14,13 +13,13 @@ export default {
 	},
 	data() {
 		return {
-			// poster_URL: `https://www.themoviedb.org/t/p/w300_and_h450_bestv2${this.movie.poster_path}`
+			// max_num : this.movie.popularity
 		}
 	},
 	methods:{
 		poster(path) {
 			return `https://www.themoviedb.org/t/p/w300_and_h450_bestv2${path}`
-		}
+		},
 	}
 }
 </script>
